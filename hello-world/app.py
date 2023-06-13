cc93fe72ac115ab457b98871e526585cfb78fbb1
Nikolaos Bellias <nikolaos.bellias@zuehlke.com>
2023-06-13 15:44:34 +0200

    Initial commit

---
+++ hello-world/app.py
@@ -0,0 +1,4 @@
+def print_message(message):
+    return message
+
+print(print_message("Hello World!"))
---
+++ hello-world/test_app.py
@@ -0,0 +1,4 @@
+from app import print_message
+
+def test_print_message():
+    assert print_message("Hello World!") == "Hello World!"
