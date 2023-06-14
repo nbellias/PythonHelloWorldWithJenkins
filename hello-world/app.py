53e05b3cc2a11edae1b1323ea895550486fe1ef6
Nikolaos Bellias <nikolaos.bellias@zuehlke.com>
2023-06-14 13:13:58 +0200

    In app.py added another deliberate error

--- hello-world/app.py
+++ hello-world/app.py
@@ -1,4 +1,4 @@
 def print_message(message):
-    return message
+    return message + "----"
 
-print(print_message("Hello World!" + " " + "I am learning Jenkins!"))
+print(print_message("Hello World!"))
